22a0daf0438062554e1202491b5e60c3a39a768d
Vitalii Borodin <v.borodin@tengebank.uz>
2025-01-08 17:47:28 +0500

    create codeVerifier & codeChallenge

--- src/main/resources/static/js/oauth2.js
+++ src/main/resources/static/js/oauth2.js
@@ -1,9 +1,18 @@
+const SHA_256 = "SHA-256";
+
 function initValues() {
 
     var state = generateState(30);
     document.getElementById("originalState").innerHTML = state;
-    console.log("state = " + state)
+    console.log("state = " + state);
 
+    var codeVerifier = generateCodeVerifier();
+    document.getElementById("codeVerifier").innerHTML = codeVerifier;
+    console.log("codeVerifier = " + codeVerifier);
+
+    generateCodeChallenge(codeVerifier).then(codeChallenge => {
+        console.log("codeChallenge = " + codeChallenge);
+    });
 }
 
 function generateState(length) {
@@ -16,4 +25,24 @@ function generateState(length) {
     }
 
     return state;
+}
+
+function generateCodeVerifier() {
+    var randomByteArray = new Uint8Array(43);
+    window.crypto.getRandomValues(randomByteArray);
+    return base64urlencode(randomByteArray);
+}
+
+function base64urlencode(sourceValue) {
+    var stringValue = String.fromCharCode.apply(null, sourceValue);
+    var base64Encoded = btoa(stringValue);
+    return base64Encoded.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+}
+
+async function generateCodeChallenge(codeVerifier) {
+    var textEncoder = new TextEncoder('US-ASCII');
+    var encodedValue = textEncoder.encode(codeVerifier);
+    var digest = await window.crypto.subtle.digest(SHA_256, encodedValue);
+
+    return base64urlencode(Array.from(new Uint8Array(digest)));
 }
